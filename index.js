79ccedea8b999f68715398e1126a2c0a9c52cb96
Samyam Prakash <samyampanda@gmail.com>
2024-06-26 22:47:04 +0530

    index.js

--- index.js
+++ index.js
@@ -2,55 +2,43 @@ var express = require("express")
 var bodyParser = require("body-parser")
 var mongoose = require("mongoose")
 
-const app = express()
-
+const app= express()
 app.use(bodyParser.json())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
-    extended:true
+    extended: true
 }))
 
-mongoose.connect('mongodb://localhost:27017/mydb',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect('mongodb://localhost:27017/MoneyList')
+var db = mongoose.connection
+db.on('error', ()=> console.log("Error in connecting to the Database"))
+db.once('open', () => console.log("Connected to Database"))
 
-var db = mongoose.connection;
+app.post("/add", (req,res) =>{
+    var category_select = req.body.category_select
+    var amount_input= req.body.amount_input
+    var info = req.body.info
+    var date_input = req.body.date_input
 
-db.on('error',()=>console.log("Error in Connecting to Database"));
-db.once('open',()=>console.log("Connected to Database"))
-
-app.post("/sign_up",(req,res)=>{
-    var name = req.body.name;
-    var email = req.body.email;
-    var phno = req.body.phno;
-    var password = req.body.password;
-
-    var data = {
-        "name": name,
-        "email" : email,
-        "phno": phno,
-        "password" : password
+    var data={
+        "Category": category_select,
+        "Amount" : amount_input,
+        "Info": info,
+        "Date": date_input
     }
-
-    db.collection('users').insertOne(data,(err,collection)=>{
+    db.collection('users').insertOne(data, (err,collection) => {
         if(err){
             throw err;
         }
-        console.log("Record Inserted Successfully");
-    });
+        console.log("Record Inserted Successfully")
 
-    return res.redirect('signup_success.html')
-
-})
-
-
-app.get("/",(req,res)=>{
-    res.set({
-        "Allow-access-Allow-Origin": '*'
     })
-    return res.redirect('index.html');
-}).listen(3000);
+})
+app.get("/",(req,res) =>{
+    res.set({
+        "Allow-access-Allow-Origin":'*'
+    })
+    return res.redirect('index.html')
+}).listen(5000)
 
-
-console.log("Listening on PORT 3000");
+console.log("Listening on port 5000")
